6a0002d9c627c4a11fd84533810509d43e01e7e9
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2016-12-20 15:07:23 +0100

    edit

--- acc/main.f90
+++ acc/main.f90
@@ -3,17 +3,17 @@ program main
   ! modules
   use omp_lib
 
-
   implicit none
 
   integer, parameter :: numthreads = 4
-  integer, parameter :: M = 100
-  integer, parameter :: L = 200
-  integer, parameter :: N = 400
+  integer, parameter :: M = 1000
+  integer, parameter :: L = 2000
+  integer, parameter :: N = 4000
 	integer :: j, i, k
   real*8 :: A(N,M), B(M, L), C(N, L)
+	real*8 :: seconds
 
-	call tic()
+  seconds = omp_get_wtime ( )
 
 	A = 3d0
 	B = 9d0
@@ -29,7 +29,8 @@ program main
 
 	write(*,*)'Done!'
 
-	call toc()
+  seconds = omp_get_wtime ( ) - seconds;
+	write(*,*)seconds
 
 	call sleep(10)
 
