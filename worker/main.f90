c9c2d8be01e76def0f224f46a74cf153f5c45ec7
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2016-12-21 18:56:56 +0100

    edit

--- worker/main.f90
+++ worker/main.f90
@@ -553,7 +553,7 @@ contains
 				ij_com = ij
 				it_com = it
 
-  	  	!$acc parallel do copyin(ij_com, it_com) collapse(3) schedule(dynamic,1) private(x, fret) num_threads(numthreads)
+  	  	!$acc parallel loop copyin(ij_com, it_com) collapse(3) schedule(dynamic,1) private(x, fret) num_threads(numthreads)
         do iw = 1, NW
           do is = 1, NS
             do ip = 0, NP
@@ -593,7 +593,7 @@ contains
             enddo ! ip
           enddo ! is
         enddo ! iw
-				!$acc end parallel do
+				!$acc end parallel loop
 
       elseif (ij == 1) then
 
