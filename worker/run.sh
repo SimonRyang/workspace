500f462613259ff502b0e20cd7cf01163f8a7e52
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2016-12-21 17:21:13 +0100

    edit

--- worker/run.sh
+++ worker/run.sh
@@ -8,10 +8,10 @@ git push origin master
 ssh -t -t flh71wr@$1 << EOF
 ulimit -s unlimited
 cd workspace/include/
-rm toolbox.f90 sorting.f90 clock.f90
+git pull origin master
 cd workspace/worker/
 git pull origin master
-mv *.f90 ~/workspace/include/*.f90 ~/workspace/worker
+cp *.f90 ~/workspace/include/*.f90 ~/workspace/worker
 rm -r Build
 rm *.mod
 $2 make
