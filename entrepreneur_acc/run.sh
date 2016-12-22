44518d91a73618329a02fa15c46376f40de8a289
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2016-12-22 10:21:17 +0100

    edit

---
+++ entrepreneur_acc/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+
+git commit -a -m "edit"
+git push origin master
+
+ssh -t -t flh71wr@$1 << EOF
+ulimit -s unlimited
+cd ~/workspace/
+git pull origin master
+cd ~/workspace/include/
+cp * ~/workspace/entrepreneur_acc/
+cd ~/workspace/entrepreneur_acc/
+rm *.mod
+rm *.o
+$2 make
+exit
+EOF
