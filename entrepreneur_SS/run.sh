8ec35a7f9d15cc51863e1351b6f9870ee12fcec0
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-01-22 16:35:15 +0100

    edit

--- entrepreneur_SS/run.sh
+++ entrepreneur_SS/run.sh
@@ -9,9 +9,6 @@ ssh -t -t -X flh71wr@$1 << EOF
 ulimit -s unlimited
 cd ~/workspace/
 git pull origin master
-cd ~/workspace/include/
-cp * ~/workspace/entrepreneur_SS/
-cd ~/workspace/entrepreneur_SS/
 $2 make
 exit
 EOF
