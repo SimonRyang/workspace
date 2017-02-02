c352735fd1f80543c2e552aee8f732d6796170e0
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-02-02 14:01:09 +0100

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -139,14 +139,6 @@ program main
                     ! calculate initial equilibrium
                     call get_SteadyState()
 
-										c_coh(:, :) = 0d0
-										a_coh(:, :) = 0d0
-										k_coh(:) = 0d0
-										inc_coh(:, :) = 0d0
-										o_coh(:, :, :) = 0d0
-										os_coh(:, :, :, :) = 0d0
-										flc_coh(:) = 0d0
-
                     share_result = sum(pop_e(:))/(sum(pop_w(:)+pop_e(:)))*100d0
                     shares_result(:, 1) = (os_coh(1, 0, 1, :)+os_coh(1, 1, 1, :))*100d0
                     shares_result(:, 2) = (os_coh(1, 0, 2, :)+os_coh(1, 1, 1, :))*100d0
