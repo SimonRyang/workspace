31ceb2208e9644a340bf9f5d4c279d1a391464e9
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-02-02 14:57:27 +0100

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -11,7 +11,7 @@ program main
   integer, parameter :: numthreads = 28
   integer :: ij
   real*8 :: shares_target(JJ, NS), shares_result(JJ, NS), share_target, share_result
-  real*8 :: sigma_val(4, NS), rho_val(4, NS), mu_val(4, NS)
+  real*8 :: sigma_val(4, NS), rho_val(4, NS), mu_val(3, NS)
   integer :: s1, s2, s3, h1, h2, h3, m1, m2, m3
 
   ! allocate arrays
@@ -112,9 +112,9 @@ program main
 
   share_target = 10.4035d0
 
-  mu_val(:, 1) = -(/0.48d0, 0.40d0, 0.32d0, 0.24d0/)
-  mu_val(:, 2) = -(/0.40d0, 0.32d0, 0.24d0, 0.16d0/)
-  mu_val(:, 3) = -(/0.16d0, 0.08d0, 0.00d0, -0.08d0/)
+  mu_val(:, 1) = -(/0.48d0, 0.40d0, 0.32d0/)
+  mu_val(:, 2) = -(/0.40d0, 0.32d0, 0.24d0/)
+  mu_val(:, 3) = -(/0.16d0, 0.00d0, -0.16d0/)
 
   rho_val(:, 1) = 1d0 - (/0.16d0, 0.08d0, 0.04d0, 0.02d0/)
   rho_val(:, 2) = 1d0 - (/0.16d0, 0.08d0, 0.04d0, 0.02d0/)
@@ -132,9 +132,9 @@ program main
         do s1 = 1, 4
           do s2 = 1, 4
             do s3 = 1, 4
-              do m1 = 1, 4
-                do m2 = 1, 4
-                  do m3 = 1, 4
+              do m1 = 1, 3
+                do m2 = 1, 3
+                  do m3 = 1, 3
 
                     ! calculate initial equilibrium
                     call get_SteadyState()
