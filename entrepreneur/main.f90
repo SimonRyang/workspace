929fada6f60070c25c7a3e54aa2555ba23ec350c
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-20 14:18:40 +0100

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -119,7 +119,7 @@ program main
   ! set reform parameters
   !pen_debt = .true.
   !smopec = .true.
-  !phi(1:TT) = 1d0
+  phi(1:TT) = 1d0
   !lambda(1:TT) = 1d0
   !mu(1:TT) = 0d0
   !labor = .false.
