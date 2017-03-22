dba9278031e58e089ad365274dc91a1e24990e5a
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-22 14:54:10 +0100

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -1029,7 +1029,7 @@ contains
       do ii = 1, size(p)
 
         if (1d0-ycum(ic) >= p(ii) .and. percentiles(ii) <= 0d0) then
-          percentiles(ii) = (xcum(ICMAX+1)-xcum(ic-1))/xcum(ICMAX+1)*100d0
+          percentiles(ii) = (xcum(ICMAX)-xcum(ic-1))/xcum(ICMAX)*100d0
         endif
 
       end do
