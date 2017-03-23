d031f214557519e1a15d56656f3968d0152b5be6
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-23 12:29:09 +0100

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -89,8 +89,8 @@ program main
 
   ! size of the asset grid
   a_l    = 0d0
-  a_u    = 1116384d0
-  a_grow = 0.1d0
+  a_u    = 16384d0
+  a_grow = 0.5d0
 
   ! size of the annuitiy grid
   x_l    = 0d0
