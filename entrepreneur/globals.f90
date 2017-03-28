64aa7cab899e0802936e23250769944a739f7b24
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-28 13:57:09 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -27,10 +27,10 @@ module globals
   integer, parameter :: NE = 5
 
   ! number of points on the asset grid (-1)
-  integer, parameter :: NA = 31
+  integer, parameter :: NA = 15
 
   ! number of points on the annuitized asset grid (-1)
-  integer, parameter :: NX = 16
+  integer, parameter :: NX = 15
 
   ! number of points on the pension claim grid (-1)
   integer, parameter :: NP = 4
@@ -279,16 +279,16 @@ contains
     ! calculate annuities
     p_hat = 0d0
     if (ij_com >= JR) then
-      temp1 = 0d0
-      do ij = ij_com, JJ
-        temp2 = 1d0
-        do iij = ij_com+1, ij
-          itj = year(it_com, ij_com, iij)
-          temp2 = temp2*(1d0+r(itj))*psix(iij, itj)
-        enddo
-        temp1 = temp1 + 1d0/temp2
+    temp1 = 0d0
+    do ij = ij_com, JJ
+      temp2 = 1d0
+      do iij = ij_com+1, ij
+        itj = year(it_com, ij_com, iij)
+        temp2 = temp2*(1d0+r(itj))*psix(iij, itj)
       enddo
-      p_hat = x(ix_com)*(1d0+r(it_com))*psix(ij_com, it_com)/temp1
+      temp1 = temp1 + 1d0/temp2
+    enddo
+    p_hat = x(ix_com)*(1d0+r(it_com))*psix(ij_com, it_com)/temp1
     endif
 
     ! calculate tommorrow's annuitized asset stock
