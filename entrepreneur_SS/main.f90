4e3b0be4ba2ed8a79aa022e83d8f38e71a6b8365
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-29 11:21:21 +0200

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -122,9 +122,9 @@ program main
 
   suc = 0.625d0
 
-  do m3 = 1, 6
-    do s3 = 1, 6
-      do h3 = 1, 6
+  do m3 = 6, 1, -1
+    do s3 = 6, 1, -1
+      do h3 = 6, 1, -1
 
         write(*,*)m3, s3, h3
 
