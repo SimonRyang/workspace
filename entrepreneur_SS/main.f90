aecd69803c62bac3f2956491aa16022edfd17083
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-03-29 16:51:53 +0200

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -120,8 +120,8 @@ program main
 
   suc = 0.625d0
 
-  do m3 = 1, 1
-    do s3 = 3, 3
+  do m3 = 5, 5
+    do s3 = 5, 5
       do h3 = 5, 5
 
         write(*,*)m3, s3, h3
@@ -201,14 +201,14 @@ program main
 !
 !    write(*,*)share_result
 
-!    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 1), color='blue')
-!    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 2), color='red')
-!    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 3), color='green')
-!
-!    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 1, :)+os_coh(1, 1, 1, :))*100d0, color='blue', linewidth=4d0)
-!    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 2, :)+os_coh(1, 1, 2, :))*100d0, color='red', linewidth=4d0)
-!    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 3, :)+os_coh(1, 1, 3, :))*100d0, color='green', linewidth=4d0)
-!    call execplot()
+    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 1), color='blue')
+    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 2), color='red')
+    call plot((/(dble(ij), ij=1,JJ)/), shares_target(:, 3), color='green')
+
+    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 1, :)+os_coh(1, 1, 1, :))*100d0, color='blue', linewidth=4d0)
+    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 2, :)+os_coh(1, 1, 2, :))*100d0, color='red', linewidth=4d0)
+    call plot((/(dble(ij), ij=1,jj)/), (os_coh(1, 0, 3, :)+os_coh(1, 1, 3, :))*100d0, color='green', linewidth=4d0)
+    call execplot()
 
   ! close files
   close(307)
