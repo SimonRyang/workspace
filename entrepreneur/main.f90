68b16b2fa39da51a9a6c344d1adaaed51ec23b62
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-04-06 15:28:28 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -787,7 +787,7 @@ contains
 
                     limit = max(1.5d0*a(ia), 1d-4)
 
-                    call fminsearch(xy, fret, (/a_l, 0d0/), (/a_u, limit/), valuefunc_e)
+                    call fminsearch(xy, fret, (/a_l, 0d0, x_l/), (/a_u, limit, x_u/), valuefunc_e)
 
                     ! copy decisions
                     aplus(1, ia, :, ip, iw, ie, is, ij, it) = xy(1)
