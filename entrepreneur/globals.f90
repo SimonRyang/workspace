76c6255f88b12a2d650d0a0f11495122a861aa51
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-04-06 12:41:08 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -108,7 +108,7 @@ module globals
   real*8, allocatable :: v(:, :, :, :, :, :, :, :, :)
 
   ! numerical variables
-  integer :: io_com, ia_com, ix_com, ip_com, iw_com, ie_com, is_com, ij_com, it_com, io_max, ix_max
+  integer :: io_com, ia_com, ix_com, ip_com, iw_com, ie_com, is_com, ij_com, it_com
   real*8 :: c_com, l_com, k_com, mx_com, xplus_com, pplus_com, oplus_com, pencon_com, inctax_com, captax_com, DIFF(0:TT)
 
   ! statistical variables
