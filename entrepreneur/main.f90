47c727db991d0f51b04694103d89ce18aad8ad10
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-04-06 15:34:17 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -783,11 +783,11 @@ contains
                     ! get initial guess for the individual choices
                     xy(1) = max(aplus(1, ia, 0, ip, iw, ie, is, ij, it), 1d-4)
                     xy(2) = max(k(1, ia, 0, ip, iw, ie, is, ij, it), 1d-4)
-                    !xy(3) = max(mx(1, ia, ix, ip, iw, ie, is, ij, it), 1d-4)
+                    xy(3) = max(mx(1, ia, ix, ip, iw, ie, is, ij, it), 1d-4)
 
                     limit = max(1.5d0*a(ia), 1d-4)
 
-                    call fminsearch(xy, fret, (/a_l, 0d0/), (/a_u, limit/), valuefunc_e)
+                    call fminsearch(xy, fret, (/a_l, 0d0, x_l/), (/a_u, limit, x_u/), valuefunc_e)
 
                     ! copy decisions
                     aplus(1, ia, :, ip, iw, ie, is, ij, it) = xy(1)
