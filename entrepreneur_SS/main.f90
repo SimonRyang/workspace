d437a03e5b001e0453d1c3cab515a0ec0e3ecc97
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-03 12:14:35 +0200

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -190,9 +190,9 @@ program main
 !    call plot((/(dble(ij), ij=1,JJ)/), a_coh(1, :))
 !    call execplot
 !
-!    call plot((/(dble(ij), ij=1,JJ)/), inc_coh(0, :))
-!    call plot((/(dble(ij), ij=1,JJ)/), inc_coh(1, :))
-!    call execplot
+    call plot((/(dble(ij), ij=1,JJ)/), inc_coh(0, :))
+    call plot((/(dble(ij), ij=1,JJ)/), inc_coh(1, :))
+    call execplot
 
 !    write(*,'(16f8.4)')shares_result(:, 1)
 !    write(*,'(16f8.4)')shares_target(:, 1)
