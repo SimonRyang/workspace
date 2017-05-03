43de3c2f389da0de6ea6cc594d0a0d7cd2b9708a
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-03 16:21:24 +0200

    edit

--- entrepreneur_SS/run.sh
+++ entrepreneur_SS/run.sh
@@ -5,16 +5,17 @@ clear
 git commit -a -m "edit"
 git push origin master
 
-ssh -t -t -X flh71wr@$1 << EOFI
+ssh -t -t -X flh71wr@$1 << EOF
 ulimit -s unlimited
 cd ~/workspace/
 git pull origin master
 cd ~/workspace/include/
 cp * ~/workspace/entrepreneur_SS/
 cd ~/workspace/entrepreneur_SS/
-rm *.mod
 rm *.o
+rm *.mod
 exit
-EOFI
+EOF
 
 cd
+echo "Test!"
