cb668d31b5060f732af3ce7c255a234eb9ef5d64
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-03 16:35:58 +0200

    edit

--- entrepreneur_SS/sshrun.sh
+++ entrepreneur_SS/sshrun.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-ulimit -s unlimited
-cd ~/workspace/
-git pull origin master
-cd ~/workspace/include/
-cp * ~/workspace/entrepreneur_SS/
-cd ~/workspace/entrepreneur_SS/
-rm *.o
-rm *.mod
-
 make
