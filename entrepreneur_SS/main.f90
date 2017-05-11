64c4d710e3729c8099e7bdc3ea31ede29660d59d
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-11 11:27:08 +0200

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -45,8 +45,8 @@ program main
   ! invert gamma
   gamma = 1d0/gamma
   sigma  =   0.320d0
-  phi1   = -17.600d0
-  phi2   =  17.600d0
+  phi1   = -19.600d0
+  phi2   =  19.600d0
   ! invert phi2
   phi2 = 1d0/phi2
   sigmaq =  1.500d0
@@ -83,7 +83,7 @@ program main
   ! size of the asset grid
   a_l    = 0d0
   a_u    = 16384d0
-  a_grow = 0.96d0
+  a_grow = 0.92d0
 
   ! size of the pension claim grid
   p_l  = 0d0
