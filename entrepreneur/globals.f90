0ba4daa07a77e4884509c858ee382fb6b7e455f7
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 13:18:17 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -144,7 +144,7 @@ contains
     real*8 :: valuefunc_w
 
     !##### OTHER VARIABLES ####################################################
-    real*8 :: a_plus, wage, v_ind, vcons_help, varpsi, varchi, varphi
+    real*8 :: a_plus, wage, v_ind, valuefunc_help, varpsi, varchi, varphi
     integer :: itp, ial, iar, ixl, ixr, ipl, ipr
 
     ! tomorrow's assets
@@ -196,22 +196,22 @@ contains
 
     ! calculate tomorrow's part of the value function and occupational decision
     valuefunc_w = 0d0
-    vcons_help = 0d0
+    valuefunc_help = 0d0
     oplus_com = 0d0
 
     if (ij_com < JJ) then
 
       ! interpolate next period's value function as a worker/retiree
-      vcons_com = interpolate_EV(a_plus, xplus_com, pplus_com, 0, iw_com, ie_com, is_com, ij_com+1, itp, 'cons')
+      valuefunc_w = interpolate_EV(a_plus, xplus_com, pplus_com, 0, iw_com, ie_com, is_com, ij_com+1, itp, 'cons')
 
       ! interpolate next period's value function as an entrepreneur
       if (ij_com < JR-1 .and. ent) then
 
-        vcons_help = interpolate_EV(a_plus, xplus_com, pplus_com, 1, iw_com, ie_com, is_com, ij_com+1, itp, 'cons') - suc
+        valuefunc_help = interpolate_EV(a_plus, xplus_com, pplus_com, 1, iw_com, ie_com, is_com, ij_com+1, itp, 'cons') - suc
 
         ! set next period's occupational decision
-        if (vcons_help > vcons_com) then
-          vcons_com = vcons_help
+        if (valuefunc_help > valuefunc_w) then
+          valuefunc_w = valuefunc_help
           oplus_com = 1d0
         endif
 
@@ -220,7 +220,11 @@ contains
     endif
 
     ! add today's part and discount
-    valuefunc_w = -(util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons_com + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq))
+    vcons_com = util(c_com, l_com) + beta*psi(is_com, ij_com+1)*interpolate_EV(a_plus, xplus_com, pplus_com, int(oplus_com), iw_com, ie_com, is_com, ij_com+1, itp, 'cons')
+    vbeq_com = (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq) !+ interpolate_EV(a_plus, xplus_com, pplus_com, oplus_com, iw_com, ie_com, is_com, ij_com+1, itp, 'beq')
+    !valuefunc_w = -(vcons_com + vbeq_com)
+    valuefunc_w = -(util(c_com, l_com) + beta*psi(is_com, ij_com+1)*valuefunc_w + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq))
+
 
   end function
 
