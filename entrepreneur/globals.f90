d289da95c9185e7dafe4f25b710527313d47f4f4
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 13:36:22 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -144,7 +144,7 @@ contains
     real*8 :: valuefunc_w
 
     !##### OTHER VARIABLES ####################################################
-    real*8 :: a_plus, wage, v_ind, vcons_help, varpsi, varchi, varphi
+    real*8 :: a_plus, wage, v_ind, vcons, vcons_help, varpsi, varchi, varphi
     integer :: itp, ial, iar, ixl, ixr, ipl, ipr
 
     ! tomorrow's assets
@@ -196,14 +196,14 @@ contains
 
     ! calculate tomorrow's part of the value function and occupational decision
     valuefunc_w = 0d0
-    vcons_com = 0d0
+    vcons = 0d0
     vcons_help = 0d0
     oplus_com = 0d0
 
     if (ij_com < JJ) then
 
       ! interpolate next period's value function as a worker/retiree
-      vcons_com = interpolate_EV(a_plus, xplus_com, pplus_com, 0, iw_com, ie_com, is_com, ij_com+1, itp, 'cons')
+      vcons = interpolate_EV(a_plus, xplus_com, pplus_com, 0, iw_com, ie_com, is_com, ij_com+1, itp, 'cons')
 
       ! interpolate next period's value function as an entrepreneur
       if (ij_com < JR-1 .and. ent) then
@@ -211,7 +211,7 @@ contains
         vcons_help = interpolate_EV(a_plus, xplus_com, pplus_com, 1, iw_com, ie_com, is_com, ij_com+1, itp, 'cons') - suc
 
         ! set next period's occupational decision
-        if (vcons_help > vcons_com) then
+        if (vcons_help > vcons) then
           vcons_com = vcons_help
           oplus_com = 1d0
         endif
@@ -221,7 +221,7 @@ contains
     endif
 
     ! add today's part and discount
-    valuefunc_w = -(util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons_com + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq))
+    valuefunc_w = -(util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq))
 
 
   end function
