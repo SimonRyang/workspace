c2dbfa18da2a845c7c1a9d0c41d37466c3661023
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 17:26:23 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -50,11 +50,11 @@ program main
   ! invert gamma
   gamma = 1d0/gamma
   sigma  =  0.320d0
-  phi1   = -9.000d0
+  phi1   = -9.500d0
   phi2   =  13.600d0
   ! invert phi2
   phi2 = 1d0/phi2
-  beta   =  0.99d0
+  beta   =  0.98d0
   ! convert variables into per period values
   beta = beta**5d0
 
@@ -68,7 +68,7 @@ program main
   delta = 1d0 - (1d0-delta)**5d0
 
   ! demographic parameters
-  n_p   = 0.064d0
+  n_p   = 0.007d0
   ! convert variables into per period values
   n_p = (1d0+n_p)**5-1d0
 
