035184aa902197093559f3c6f78449275b0e324c
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 14:16:00 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -228,11 +228,11 @@ contains
     endif
 
     ! add today's part and discount
-    vcons = util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons
+    vcons = util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq)
     vbeq = (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq) +  beta*psi(is_com, ij_com+1)*vbeq
     vcons_com = vcons
     vbeq_com = vbeq
-    valuefunc_w = -(vcons + vbeq)
+    valuefunc_w = -(vcons)
 
   end function
 
@@ -452,11 +452,11 @@ contains
     endif
 
     ! add today's part and discount
-    vcons = util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons
+    vcons = util(c_com, l_com) + beta*psi(is_com, ij_com+1)*vcons + (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq)
     vbeq = (1d0-psi(is_com, ij_com+1))*phi1*(1d0+a_plus*phi2)**(1d0-sigmaq) + beta*psi(is_com, ij_com+1)*vbeq
     vcons_com = vcons
     vbeq_com = vbeq
-    valuefunc_r = -(vcons + vbeq)
+    valuefunc_r = -(vcons)
 
   end function
 
