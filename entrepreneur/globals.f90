3bd8d08453f3eac414251fe2602dba74949da4a4
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 09:49:40 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -27,10 +27,10 @@ module globals
   integer, parameter :: NE = 5
 
   ! number of points on the asset grid (-1)
-  integer, parameter :: NA = 32
+  integer, parameter :: NA = 128
 
   ! number of points on the annuitized asset grid (-1)
-  integer, parameter :: NX = 2
+  integer, parameter :: NX = 1
 
   ! number of points on the pension claim grid (-1)
   integer, parameter :: NP = 5
