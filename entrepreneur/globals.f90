7cc10c8104f92b6d578021f54b05f8f6294029a7
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-15 12:26:03 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -578,6 +578,7 @@ contains
 
         ! interpolate expected valuefunction
         if (type == 'cons') then
+          write(*,*)'hell no'
           interpolate_EV = (varphi*varchi*varpsi*EV_cons(io, ial, ixl, ipl, iw, ie, is, ij, it) &
                             + varphi*varchi*(1d0-varpsi)*EV_cons(io, ial, ixl, ipr, iw, ie, is, ij, it) &
                             + varphi*(1d0-varchi)*varpsi*EV_cons(io, ial, ixr, ipl, iw, ie, is, ij, it) &
