2039b3abc8cd91103f85654da085e8381421de91
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-16 14:41:06 +0200

    edit

--- entrepreneur/run.sh
+++ entrepreneur/run.sh
@@ -6,7 +6,6 @@ git commit -a -m "edit"
 git push origin master
 
 ssh -t -t -X flh71wr@$1 << EOF
-ulimit -s unlimited
 cd ~/workspace/
 git pull origin master
 cd ~/workspace/include/
--- entrepreneur/sshrun.sh
+++ entrepreneur/sshrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ulimit -s unlimited
 source ~/intel/compilers_and_libraries_2017/linux/bin/compilervars.sh intel64
 cd ~/workspace/entrepreneur/
 make
