ba078bb774a8553104b40c6db3f77659ee7f2c0f
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-19 10:49:43 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -50,7 +50,7 @@ program main
   ! invert gamma
   gamma = 1d0/gamma
   sigma  =  0.320d0
-  mu_b   =  -1.500d0
+  mu_b   =  -0.500d0
   beta   =  0.987d0
   ! convert variables into per period values
   beta = beta**5d0
