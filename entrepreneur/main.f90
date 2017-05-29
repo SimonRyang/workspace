a8014ca238961f0bf7d4efe7c778f0cd3a24fc27
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-29 13:57:41 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -101,7 +101,7 @@ program main
   ! simulation parameters
   damp  = 0.5d0
   tol   = 1d-4
-  itermax = 200
+  itermax = 2
 
   ! compute gini
   gini_on = .true.
@@ -366,7 +366,7 @@ contains
     ! compute survival probabilities for high/low skilled
     psi(:, 1) = psi(2, 1)
     psi(:, JJ+1) = 0d0
-    adj = 22d0
+    adj = 24d0
     do ij = 2, JJ
       psi(1, ij) = psi(2, ij) - exp(0.33d0*(dble(ij-1)-adj))
       psi(3, ij) = psi(2, ij) + exp(0.33d0*(dble(ij-1)-adj))
