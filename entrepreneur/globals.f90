183b437ea6016416336543e67a710e31b64afb3f
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-30 10:56:34 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -33,7 +33,7 @@ module globals
   integer, parameter :: NX = 2
 
   ! number of points on the pension claim grid (-1)
-  integer, parameter :: NP = 5
+  integer, parameter :: NP = 6
 
   ! number of occupations (-1)
   integer, parameter :: NO = 1
