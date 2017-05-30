50d426c7c9273852a3ccc75bf57c118056f77289
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-30 15:30:26 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -61,7 +61,7 @@ program main
   nu = 0.88d0
   l_bar = 0.47d0
   suc = (1d0 - 0.42d0)**(1d0-gamma) !0.625d0
-  swc = (1d0 - 0.42d0)**(1d0-gamma) !1d0 + (suc - 1d0)/2d0
+  swc = (1d0 - 0.48d0)**(1d0-gamma) !1d0 + (suc - 1d0)/2d0
   ! convert variables into per period values
   delta = 1d0 - (1d0-delta)**5d0
 
