ef3516e4a1676e992a3e57c3896d3651b95dbe08
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-05-31 14:23:31 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -90,8 +90,8 @@ program main
 
   ! size of the annuitiy grid
   x_l    = 0d0
-  x_u    = 32d0
-  x_grow = 0.2d0
+  x_u    = 128d0
+  x_grow = 0.5d0
 
   ! size of the pension claim grid
   p_l  = 0d0
