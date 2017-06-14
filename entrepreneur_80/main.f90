83c60a1eab31ddf4923caaeaa1e66bc671f53421
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-06-14 11:19:00 +0200

    edit

--- entrepreneur_80/main.f90
+++ entrepreneur_80/main.f90
@@ -383,7 +383,8 @@ contains
     Gama(7) = 1.8d0*pop(7, 0)
     Gama(8) = 1.8d0*pop(8, 0)
     Gama(9) = 1.6d0*pop(9, 0)
-    Gama(1:JR-1) = 1d0
+    Gama(1:19) = 0d0
+    Gama(20:JR-1) = 1d0
     Gama(JR:JJ) = 0d0
     Gama = Gama/sum(Gama)
 
