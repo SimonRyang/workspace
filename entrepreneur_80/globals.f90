f851c323343c30327b9747c27493ab3c79094c76
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-06-22 14:21:49 +0200

    edit

--- entrepreneur_80/globals.f90
+++ entrepreneur_80/globals.f90
@@ -21,10 +21,10 @@ module globals
   integer, parameter :: NS = 3
 
   ! number of transitory shock process values (worker)
-  integer, parameter :: NW = 5
+  integer, parameter :: NW = 7
 
   ! number of transitory shock process values (entrepreneur)
-  integer, parameter :: NE = 5
+  integer, parameter :: NE = 7
 
   ! number of points on the asset grid (-1)
   integer, parameter :: NA = 24
