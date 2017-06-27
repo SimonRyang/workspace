b9d4793042db7cc2dcb7bb265bf3d573f1917695
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-06-27 08:46:41 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -88,8 +88,8 @@ program main
 
   ! size of the asset grid
   a_l    = 0d0
-  a_u    = 32d0
-  a_grow = 0.6d0
+  a_u    = 64d0
+  a_grow = 0.4d0
 
   ! size of the annuitiy grid
   x_l    = 0d0
@@ -110,11 +110,11 @@ program main
 
   ! set switches
   !ann = .true.
-  !ent = .false.
+  ent = .false.
 
   ! calculate initial equilibrium
   call get_SteadyState()
-
+stop
   ! set reform parameters
   !ann = .true.
   pen_debt = .true.
@@ -417,10 +417,10 @@ contains
     call discretize_AR(0.9400d0**5d0, -0.600d0, sigma5(0.9400d0, 0.0370d0), theta(:, 1), pi_theta(:, :, 1), dist_theta(:, 1))
     theta(:, 1) = exp(theta(:, 1))!/sum(dist_theta(:, 1)*exp(theta(:, 1)))
 
-    call discretize_AR(0.9400d0**5d0, -0.550d0, sigma5(0.9400d0, 0.0380d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
+    call discretize_AR(0.9400d0**5d0, -0.500d0, sigma5(0.9400d0, 0.0380d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
     theta(:, 2) = exp(theta(:, 2))!/sum(dist_theta(:, 2)*exp(theta(:, 2)))
 
-    call discretize_AR(0.9410d0**5d0, -0.300d0, sigma5(0.9410d0, 0.0395d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
+    call discretize_AR(0.9410d0**5d0, -0.250d0, sigma5(0.9410d0, 0.0395d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
     theta(:, 3) = exp(theta(:, 3))!/sum(dist_theta(:, 3)*exp(theta(:, 3)))
 
     ! initial guesses for macro variables
