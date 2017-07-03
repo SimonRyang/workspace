54322b69edc3bb6cf656162a332af58aa01dd60e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-03 15:20:03 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -111,13 +111,11 @@ program main
   ! calculate initial equilibrium
   call get_SteadyState()
 
-stop
-
   ! set reform parameters
-  !ann = .true.
-  pen_debt = .true.
+  ann = .true.
+  !pen_debt = .true.
   !smopec = .true.
-  phi(1:TT) = 1d0
+  !phi(1:TT) = 1d0
   !lambda(1:TT) = 1d0
   !mu(1:TT) = 0d0
   !labor = .false.
