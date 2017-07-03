3accd9a91c9afe00dc2b0d5b86813f33d19699ed
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-03 08:47:44 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -413,10 +413,10 @@ contains
     call discretize_AR(0.9390d0**5d0, -0.600d0, sigma5(0.9390d0, 0.0365d0), theta(:, 1), pi_theta(:, :, 1), dist_theta(:, 1))
     theta(:, 1) = exp(theta(:, 1))!/sum(dist_theta(:, 1)*exp(theta(:, 1)))
 
-    call discretize_AR(0.9360d0**5d0, -0.550d0, sigma5(0.9360d0, 0.0375d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
+    call discretize_AR(0.9360d0**5d0, -0.550d0, sigma5(0.9360d0, 0.0370d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
     theta(:, 2) = exp(theta(:, 2))!/sum(dist_theta(:, 2)*exp(theta(:, 2)))
 
-    call discretize_AR(0.9450d0**5d0, -0.300d0, sigma5(0.9450d0, 0.0385d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
+    call discretize_AR(0.9450d0**5d0, -0.305d0, sigma5(0.9450d0, 0.0390d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
     theta(:, 3) = exp(theta(:, 3))!/sum(dist_theta(:, 3)*exp(theta(:, 3)))
 
     ! initial guesses for macro variables
