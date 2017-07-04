1e4f85bc35ae0e0de3869df3c3b900f5da8db34c
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-04 19:17:07 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -98,14 +98,14 @@ program main
 
   ! simulation parameters
   damp  = 0.5d0
-  tol   = 1d-8
+  tol   = 1d-6
   itermax = 200
 
   ! compute gini
   gini_on = .true.
 
   ! set switches
-  !ann = .true.
+  ann = .true.
   !ent = .false.
 
   ! calculate initial equilibrium
