9709be4353c78cb8098a11641754712fdb36f723
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-05 10:34:49 +0200

    edit

--- entrepreneur_SS/main.f90
+++ entrepreneur_SS/main.f90
@@ -824,7 +824,7 @@ contains
     do is = 1, NS
       do ie = 1, NE
         do iw = 1, NW
-          m(0, 0, 0, iw, 3, is, 1) = dist_skill(is)*dist_eta(iw, is) !dist_eta(iw, is)*dist_theta(ie, is)*dist_skill(is)
+          m(0, 0, 0, iw, 1, is, 1) = dist_skill(is)*dist_eta(iw, is) !dist_eta(iw, is)*dist_theta(ie, is)*dist_skill(is)
         enddo ! iw
       enddo ! ie
     enddo ! is
