9a4bdc92013feae445779b6fa9e1057b795d9cbb
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-05 15:34:01 +0200

    edit

--- entrepreneur_SS/globals.f90
+++ entrepreneur_SS/globals.f90
@@ -30,7 +30,7 @@ module globals
   integer, parameter :: NP = 5
 
   ! number of occupations (-1)
-  integer, parameter :: NO = 0
+  integer, parameter :: NO = 1
 
   ! household parameters
   real*8 :: gamma, sigma, mu_b, beta, l_bar
