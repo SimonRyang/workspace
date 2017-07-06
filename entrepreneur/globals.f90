f9e90b02bc54dff4d41fb5f3bda0cefdea32013b
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-07-06 15:06:15 +0200

    edit

--- entrepreneur/globals.f90
+++ entrepreneur/globals.f90
@@ -488,7 +488,7 @@ contains
     real*8 :: profent
 
     ! compute profit
-    profent = theta(ie, is)*(k**alpha*(eff(ij, is)*l_bar)**(1d0-alpha))**nu - delta*k! - r(it)*max(k-a(ia), 0d0)
+    profent = theta(ie, is)*(k**alpha*(eff(ij, is)*l_bar)**(1d0-alpha))**nu !- delta*k - r(it)*max(k-a(ia), 0d0)
 
   end function
 
