4f338b199ad3ab34a7681e033dc000a48a2d6a5b
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-08-18 13:14:18 +0200

    edit

--- entrepreneur/main.f90
+++ entrepreneur/main.f90
@@ -417,13 +417,13 @@ contains
     eta(:, 3) = exp(eta(:, 3))/sum(dist_eta(:, 3)*exp(eta(:, 3)))
 
     ! initialize entrepreneurial ability
-    call discretize_AR(0.939d0**5d0, -0.600d0, sigma5(0.939d0, 0.0370d0), theta(:, 1), pi_theta(:, :, 1), dist_theta(:, 1))
+    call discretize_AR(0.910d0**5d0, -0.100d0, sigma5(0.910d0, 0.0350d0), theta(:, 1), pi_theta(:, :, 1), dist_theta(:, 1))
     theta(:, 1) = exp(theta(:, 1))!/sum(dist_theta(:, 1)*exp(theta(:, 1)))
 
-    call discretize_AR(0.936d0**5d0, -0.550d0, sigma5(0.936d0, 0.0380d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
+    call discretize_AR(0.920d0**5d0, -0.100d0, sigma5(0.920d0, 0.0375d0), theta(:, 2), pi_theta(:, :, 2), dist_theta(:, 2))
     theta(:, 2) = exp(theta(:, 2))!/sum(dist_theta(:, 2)*exp(theta(:, 2)))
 
-    call discretize_AR(0.945d0**5d0, -0.300d0, sigma5(0.945d0, 0.0390d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
+    call discretize_AR(0.945d0**5d0, -0.150d0, sigma5(0.945d0, 0.0550d0), theta(:, 3), pi_theta(:, :, 3), dist_theta(:, 3))
     theta(:, 3) = exp(theta(:, 3))!/sum(dist_theta(:, 3)*exp(theta(:, 3)))
 
     ! initial guesses for macro variables
