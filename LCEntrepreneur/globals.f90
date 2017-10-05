460a8842ef7d9fdbd4ba6d11fa8f9da51755d08f
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-05 21:39:50 +0200

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -50,7 +50,7 @@ module globals
     real*8, parameter :: xi = 1d0/3d0
 
     ! production parameters
-    real*8, parameter :: k_min = 0d0
+    real*8, parameter :: k_min = 0.2d0
     real*8, parameter :: phi_k = 0.2d0
     real*8, parameter :: alpha = 0.36d0
     real*8, parameter :: nu = 0.88d0
