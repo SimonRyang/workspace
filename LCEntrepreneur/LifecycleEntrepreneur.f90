a3941048d89b6f76e3e579b56a7cfa6f6dc6e4f1
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-05 20:19:31 +0200

    edit

--- LCEntrepreneur/LifecycleEntrepreneur.f90
+++ LCEntrepreneur/LifecycleEntrepreneur.f90
@@ -84,7 +84,7 @@ contains
         eta = exp(eta)
 
         ! discretize theta shocks
-        call discretize_AR(rho, 0d0, sigma_theta, theta, pi_theta, dist_theta)
+        call discretize_AR(rho, -10d0, sigma_theta, theta, pi_theta, dist_theta)
         theta = exp(theta)
 
         ! initialize asset grid
