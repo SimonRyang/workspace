f16099ce7a8020dd77fbe5fa00aa3a503e4a37eb
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-09 15:53:21 +0200

    edit

--- OLGHouse/OLGHouse.f90
+++ OLGHouse/OLGHouse.f90
@@ -544,6 +544,8 @@ subroutine get_distribution()
            enddo
        enddo
 
+       write(*,*)sum(phi(ij, :, :, :, :, :)
+
        if (sum(phi(ij, :, :, :, :, :)) < 1d0-1d10)write(*,'(a, i5, f8.2)')'X: ', ij, sum(phi(ij, :, :, :, :, :))
 
    enddo
