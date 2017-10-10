95e670627775b283237394272b55b402bd88e283
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-10 09:54:10 +0200

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -43,7 +43,7 @@ module globals
     real*8, parameter :: egam = 1d0 - 1d0/gamma
     real*8, parameter :: sigma = 0.3d0
     real*8, parameter :: beta = 0.99d0**5d0
-    real*8, parameter :: mu_b = 0.1d0
+    real*8, parameter :: mu_b = 0.0d0
     ! convert variables into per period values
 
     ! risk free rate and risk premium
