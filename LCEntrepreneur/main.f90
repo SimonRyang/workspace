41433ad5ef64761f48c7973e73cb25c07c896f4e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-11 16:56:32 +0200

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -227,6 +227,10 @@ contains
 
             call interpolate(ij)
 
+            write(*,*)sum(V(ij, :, :, :, :, :))
+            write(*,*)sum(c(ij, :, :, :, :, :))
+            write(*,*)sum(a_plus(ij, :, :, :, :, :))
+
             write(*,'(a,i3,a)')'Age: ',ij,' DONE!'
 
 
