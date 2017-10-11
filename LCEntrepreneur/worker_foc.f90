fd3e3982309693499f746418d5893b8b0746756e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-11 14:37:42 +0200

    edit

--- LCEntrepreneur/worker_foc.f90
+++ LCEntrepreneur/worker_foc.f90
@@ -22,23 +22,29 @@ contains
       real*8, intent(in) :: x_in(:)
 
       ! variable declarations
-      real*8 :: cons_w, X_plus, wage, profit, tomorrow, varphi_x, varphi_p
+      real*8 :: cons_w, X_plus, income, tomorrow, varphi_x, varphi_p
       integer :: ixl_p, ixr_p, ipl_p, ipr_p
 
       ! calculate tomorrow's assets
       X_plus  = x_in(1)
       lab_com = x_in(2)
 
+      income = abs(dble(ik==0))*w*eff(ij_com)*eta(iw_com)*lab_com + &
+               (1d0-abs(dble(ik==0)))*theta(ie_com)*(k(ik_com)**alpha*(eff(ij_com)*lab_com)**(1d0-alpha))**nu + (1d0-delta_k)*k(ik_com)
+
+
       ! maximize value function for current worker (next period worker)
       if (ik_com == 0) then
 
-          cons_com = (1d0+r)*a(ia_com) + w*eff(ij_com)*eta(iw_com)*lab_com + pen(ij_com, ip_com) - X_plus
+
+
+          cons_com = (1d0+r)*a(ia_com) + income + pen(ij_com, ip_com) - X_plus
           p_plus_com = (p(ip_com)*dble(ij_com-1) + mu*(lambda + (1d0-lambda)*min(w*eff(ij_com)*eta(iw_com)*lab_com, p_u)))/dble(ij_com)
 
       ! maximize value function for current entrepreneur (next period worker)
       else
 
-          cons_com = (1d0+r)*(a(ia_com)-xi*k(ik_com)) + theta(ie_com)*(k(ik_com)**alpha*(eff(ij_com)*lab_com)**(1d0-alpha))**nu + (1d0-delta_k)*k(ik_com) + pen(ij_com, ip_com) - X_plus
+          cons_com = (1d0+r)*(a(ia_com)-xi*k(ik_com)) + income + pen(ij_com, ip_com) - X_plus
           p_plus_com = (p(ip_com)*dble(ij_com-1) + phi*mu*(lambda + (1d0-lambda)*min(theta(ie_com)*(k(ik_com)**alpha*(eff(ij_com)*lab_com)**(1d0-alpha))**nu, p_u)))/dble(ij_com)
 
       endif
