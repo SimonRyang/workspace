f6c44adaa00398255afb14f0fcc35ff39c0496fc
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-12 15:01:19 +0200

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -207,8 +207,8 @@ contains
                              V(ij, ia, ip, ik, iw, ie) = V_t(ij, ia, ip, ik, iw, ie, 0)
                            endif
 
-                           if (ij < JR-1 .and. k_plus(ij, ia, ip, ik, iw, ie) > 0d0 .and. k_plus(ij, ia, ip, ik, iw, ie) < k_min)write(*,*)k_min, k_plus(ij, ia, ip, ik, iw, ie), X_plus(ij, ia, ip, ik, iw, ie)
-                           if (ij < JR-1 .and. k_plus(ij, ia, ip, ik, iw, ie) > 0d0 .and. X_plus(ij, ia, ip, ik, iw, ie) < (1d0-xi)*k_min)write(*,*)k_min, k_plus(ij, ia, ip, ik, iw, ie), X_plus(ij, ia, ip, ik, iw, ie)
+                           if (k_plus(ij, ia, ip, ik, iw, ie) > 0d0 .and. k_plus(ij, ia, ip, ik, iw, ie) < k_min)write(*,*)k_min, k_plus(ij, ia, ip, ik, iw, ie), X_plus(ij, ia, ip, ik, iw, ie)
+                           if (k_plus(ij, ia, ip, ik, iw, ie) > 0d0 .and. X_plus(ij, ia, ip, ik, iw, ie) < (1d0-xi)*k_min)write(*,*)k_min, k_plus(ij, ia, ip, ik, iw, ie), X_plus(ij, ia, ip, ik, iw, ie)
                        enddo
                      enddo
                    enddo
