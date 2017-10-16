13e7896846682be10fbd3fedaf6a2d9e9e08c743
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-16 17:03:38 +0200

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -11,10 +11,10 @@ module globals
     integer, parameter :: JJ = 16
 
     ! number of productivity (eta) shocks
-    integer, parameter :: NW = 5
+    integer, parameter :: NW = 9
 
     ! number of entrepreneurial ability (theta) shocks
-    integer, parameter :: NE = 5
+    integer, parameter :: NE = 9
 
     ! number of points on the asset grid
     integer, parameter :: NX = 32
