f6f417c9cd0436e92683e761f41c94b584dee555
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-20 10:18:31 +0200

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -209,12 +209,7 @@ module globals
       x_in(2) = max(l_t(ij+1, ia, ip, ik, iw, ie, io_p), 0.33d0)
 
       ! solve the household problem using rootfinding
-      if (ij<JR) then
-        call fminsearch(x_in, fret, (/X_l, 0d0/), (/X_u, 0.99d0/), value_func)
-      else
-        call fminsearch((/x_in(1)/), fret, (/X_l/), (/X_u/), value_func)
-      endif
-
+      call fminsearch(x_in, fret, (/X_l, 0d0/), (/X_u, 0.99d0/), value_func)
 
       ! determine future investment
       k_p = 0d0
