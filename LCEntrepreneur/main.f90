aa365a6aa46232a7abf425845460b8a357abb267
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-10-24 15:12:18 +0200

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -73,7 +73,6 @@ contains
 
         ! discretize theta shocks
         call discretize_AR(0.920d0**5d0, 0.0d0, sigma5(0.920d0, 0.0375d0), theta, pi_theta, dist_theta)
-
         theta = exp(theta)
 
         ! initialize asset grid
@@ -182,7 +181,8 @@ contains
                                  call solve_worker(ij, iq_p, ix, ip_p, ik, iw, ie)
 
                                  ! next period entrepreneur
-                                 if(.false.)call solve_entrepreneur(ij, iq_p, ix, ip_p, ik, iw, ie)
+                                 call solve_entrepreneur(ij, iq_p, ix, ip_p, ik, iw, ie)
+
                                endif
                             enddo
                          enddo
@@ -205,10 +205,10 @@ contains
                            call solve_consumption(ij, ia, ix, ip, ik, iw, ie, 0)
 
                            ! next period entrpreneur
-                           if(ij<JR-1 .and. .false.)call solve_consumption(ij, ia, ix, ip, ik, iw, ie, 1)
+                           if(ij<JR-1)call solve_consumption(ij, ia, ix, ip, ik, iw, ie, 1)
 
                            ! decision on whether to be homeowner or renter next period
-                            if(ij < JR-1 .and. .false. .and. V_t(ij, ia, ix, ip, ik, iw, ie, 1) > V_t(ij, ia, ix, ip, ik, iw, ie, 0)) then
+                            if(ij < JR-1 .and. V_t(ij, ia, ix, ip, ik, iw, ie, 1) > V_t(ij, ia, ix, ip, ik, iw, ie, 0)) then
                                   Q_plus(ij, ia, ix, ip, ik, iw, ie) = Q_plus_t(ij, ia, ix, ip, ik, iw, ie, 1)
                                   a_plus(ij, ia, ix, ip, ik, iw, ie) = a_plus_t(ij, ia, ix, ip, ik, iw, ie, 1)
                                   x_plus(ij, ia, ix, ip, ik, iw, ie) = x_plus_t(ij, ia, ix, ip, ik, iw, ie, 1)
