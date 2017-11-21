62561ab3c818f3b5f6e3105ab062cc8c13f104f9
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-21 17:56:17 +0100

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -453,7 +453,7 @@ contains
             y_coh(1, ij) = y_coh(1, ij)/max(sum(m(:, 1:NK, :, :, :, :, ij)), 1d-16)
             l_coh(0, ij) = l_coh(0, ij)/max(sum(m(:, 0, :, :, :, :, ij)), 1d-16)
             l_coh(1, ij) = l_coh(1, ij)/max(sum(m(:, 1:NK, :, :, :, :, ij)), 1d-16)
-            o_coh(ij) = o_coh(ij)/max(sum(m(ij, :, :, :, :, :, :)), 1d-16)
+            o_coh(ij) = o_coh(ij)/max(sum(m(:, :, :, :, :, :, ij)), 1d-16)
             k_coh(ij) = k_coh(ij)/max(sum(m(:, 1:NK, :, :, :, :, ij)), 1d-16)
 
         enddo
