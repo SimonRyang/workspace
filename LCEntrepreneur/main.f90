f2a365b21b74c7a6f260c0271b3ae5235e8fd133
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-22 13:18:21 +0100

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -167,7 +167,7 @@ contains
 
                ! get optimal share of wealth invested into capital
 
-                  !$omp parallel do collapse(2) schedule(dynamic) num_threads(numthreads) shared(ij) default(none)
+                  !$omp parallel do collapse(3) schedule(dynamic) num_threads(numthreads) shared(ij) default(none)
                    do ie = 1, NE
                      do iw = 1, NW
                        do ip_p = 0, NP
