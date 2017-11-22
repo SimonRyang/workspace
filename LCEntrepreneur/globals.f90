e82b31455904b464183a80a1fc9ec4443868b4de
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-22 15:09:29 +0100

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -11,10 +11,10 @@ module globals
     integer, parameter :: JJ = 16
 
     ! number of productivity (eta) shocks
-    integer, parameter :: NW = 5
+    integer, parameter :: NW = 3
 
     ! number of entrepreneurial ability (theta) shocks
-    integer, parameter :: NE = 5
+    integer, parameter :: NE = 3
 
     ! number of points on the asset grid
     integer, parameter :: NQ = 16
@@ -297,7 +297,7 @@ module globals
       else
 
         x_p = x(ix)
-        
+
       endif
 
       ! copy decisions
--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -78,9 +78,7 @@ contains
         ! discretize theta shocks
         call discretize_AR(0.920d0**5d0, 0.0d0, sigma5(0.920d0, 0.0375d0), theta, pi_theta, dist_theta)
         theta = exp(theta)
-
 theta = 0d0
-
         ! initialize asset grid
         call grid_Cons_Grow(Q, Q_l, Q_u, Q_grow)
 
@@ -106,6 +104,8 @@ theta = 0d0
         do ix = 0, NX
           ann(ix, JR:JJ) = (1d0+r)/psi(JR)*x(ix)/ann_temp
         enddo
+
+        ann = 0d0
 
         ! old-age transfers
         pen = 0d0
