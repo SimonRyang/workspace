3dadb35963fef695f4c5ba528ae5a2af10e2ff14
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-22 13:15:55 +0100

    edit

--- LCEntrepreneur/sshrun.sh
+++ LCEntrepreneur/sshrun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ulimit -s unlimited
-source ~/intel/compilers_and_libraries_2017/linux/bin/compilervars.sh intel64
+source ~/intel/compilers_and_libraries_2018/linux/bin/compilervars.sh intel64
 cd ~/workspace/LCEntrepreneur/
 $1 make -B
