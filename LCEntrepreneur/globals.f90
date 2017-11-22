443fc033be67f6ecba014ced6aeccf361797e2ad
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-22 15:22:58 +0100

    edit

--- LCEntrepreneur/globals.f90
+++ LCEntrepreneur/globals.f90
@@ -36,7 +36,7 @@ module globals
     real*8, parameter :: egam = 1d0 - 1d0/gamma
     real*8, parameter :: sigma = 0.3d0
     real*8, parameter :: beta = 0.99d0
-    real*8, parameter :: mu_b = 0.0d0
+    real*8, parameter :: mu_b = 0.1d0
 
     ! risk free rate and risk premium
     real*8, parameter :: r  = 0.1d0
