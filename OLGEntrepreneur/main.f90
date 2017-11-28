b4dbd777afe1866857d6714af77b255fe790ecb8
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-28 14:00:15 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -324,6 +324,7 @@ contains
                              if (ij >= JR) then
                                write(*,*)ia, ik, ix, ip, iw, ie, ij
                                write(*,*) (1d0+r)*(a(ia)-xi*k(ik)) + ann(ix, ij) + pen(ip, ij) - a_plus(ia, ik, ix, ip, iw, ie, ij) - c(ia, ik, ix, ip, iw, ie, ij)
+                               write(*,*) Q_plus(ia, ik, ix, ip, iw, ie, ij), a_plus(ia, ik, ix, ip, iw, ie, ij)
                                write(*,*) k_plus(ia, ik, ix, ip, iw, ie, ij), x_plus(ia, ik, ix, ip, iw, ie, ij)
                                call sleep(1)
                             endif
