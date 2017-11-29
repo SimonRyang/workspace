860aec6ceceb067940036fbaf5cbe6c59c80c4ba
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-29 14:20:28 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -84,7 +84,7 @@ module globals
     ! numerical parameters
     integer, parameter :: itermax = 200
     real*8, parameter :: sig = 1d-6
-    real*8, parameter :: damp = 0.10d0
+    real*8, parameter :: damp = 0.75d0
 
     ! measure time
     integer :: time
