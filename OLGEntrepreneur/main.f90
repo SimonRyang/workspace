f95fad6c50b9cded77171179cb538859b9c998b0
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-11-30 22:05:10 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -12,7 +12,7 @@ program main
     integer, parameter :: numthreads = 24
 
     ! set government variables
-    mu     = 1d0
+    mu     = 0d0
     lambda = 0d0
     phi    = 0d0
 
@@ -118,7 +118,7 @@ contains
         call discretize_AR(0.920d0**5d0, 0.0d0, sigma5(0.920d0, 0.0375d0), theta, pi_theta, dist_theta)
         theta = exp(theta)
 
-        theta = 0d0
+        !theta = 0d0
 
         ! initialize asset grid
         call grid_Cons_Grow(Q, Q_l, Q_u, Q_grow)
