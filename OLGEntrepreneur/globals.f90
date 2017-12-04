37b9b0995afaf2be5b905d29171c715324fec41b
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 15:29:52 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -57,7 +57,7 @@ module globals
 
     ! size of the asset grid
     real*8, parameter :: Q_l    = 0d0
-    real*8, parameter :: Q_u    = 4d0
+    real*8, parameter :: Q_u    = 6d0
     real*8, parameter :: Q_grow = 0.05d0
 
     ! size of the liquid asset grid
@@ -85,7 +85,7 @@ module globals
     ! numerical parameters
     integer, parameter :: itermax = 200
     real*8, parameter :: sig = 1d-6
-    real*8, parameter :: damp = 0.50d0
+    real*8, parameter :: damp = 0.45d0
 
     ! measure time
     integer :: time
