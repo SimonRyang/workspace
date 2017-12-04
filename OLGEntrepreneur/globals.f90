7477800363f7faa7dd3ff8dcf75ebbcb5e01b8d6
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 16:21:01 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -58,7 +58,7 @@ module globals
     ! size of the asset grid
     real*8, parameter :: Q_l    = 0d0
     real*8, parameter :: Q_u    = 4d0
-    real*8, parameter :: Q_grow = 0.25d0
+    real*8, parameter :: Q_grow = 0.025d0
 
     ! size of the liquid asset grid
     real*8, parameter :: a_l    = Q_l
