5fc6aa87015fd88523a1b8aeecfee6eb854ccf50
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 10:56:35 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -152,9 +152,9 @@ contains
         taup  = 0.164d0
 
         ! set starting values
-        KC = 4.00d0
+        KC = 10.00d0
         LC = 3.00d0
-        BQ = 0.40d0
+        BQ = 1.40d0
 
         ! initial guess bequests
         b = 0d0
