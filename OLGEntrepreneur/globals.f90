00b23134bac72ff58866e3301ddcdec8db3c5ec2
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 09:45:47 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -23,7 +23,7 @@ module globals
     integer, parameter :: NA = 12
 
     ! number of points on the capital grid
-    integer, parameter :: NK = 12
+    integer, parameter :: NK = 1
 
     ! number of points on the annuity asset grid
     integer, parameter :: NX = 1
