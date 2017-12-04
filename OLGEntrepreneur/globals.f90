6d6429fba30569426fe1b97ba387eb9d59fc26a9
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 12:13:26 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -82,7 +82,7 @@ module globals
     real*8, parameter :: kappa = 0.45d0
 
     ! numerical parameters
-    integer, parameter :: itermax = 200
+    integer, parameter :: itermax = 20000
     real*8, parameter :: sig = 1d-6
     real*8, parameter :: damp = 0.010d0
 
