dff7b5127e8aff7771a4688c9e27b36cde7bd150
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 09:55:13 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -32,7 +32,7 @@ module globals
     integer, parameter :: NP = 4
 
     ! demographic parameters
-    real*8, parameter :: n_p = 0d0 !(1d0+0.005d0)**5-1d0
+    real*8, parameter :: n_p = (1d0+0.005d0)**5-1d0
 
     ! household preference parameters
     real*8, parameter :: gamma = 0.5d0
@@ -56,7 +56,7 @@ module globals
 
     ! size of the asset grid
     real*8, parameter :: Q_l    = 0d0
-    real*8, parameter :: Q_u    = 4d0
+    real*8, parameter :: Q_u    = 6d0
     real*8, parameter :: Q_grow = 0.05d0
 
     ! size of the liquid asset grid
