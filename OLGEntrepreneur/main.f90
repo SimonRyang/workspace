5772ff2bd7c6c402d807b5241a650671064ac28f
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-04 15:40:48 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -643,6 +643,8 @@ contains
         ! get budget balancing pension contribution rate
         taup = PBEN/PCON
 
+        taup = damp*taup + (1d0-damp)*taup_old
+
         write(*,*) PBEN, PCON, taup
 
     end subroutine
