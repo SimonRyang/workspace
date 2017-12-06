89fdf7b713db2c63e5a7989b7db83735261ac3d8
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-06 14:22:20 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -109,11 +109,7 @@ contains
 
         ! discretize eta shocks
         call discretize_AR(0.95687d0**5d0, 0.0d0, sigma5(0.95687d0, 0.02812d0), eta, pi_eta, dist_eta)
-        eta = exp(eta)/sum(dist_eta*exp(eta))
-
-        write(*,*)eta
-        write(*,*)dist_eta
-        write(*,*)sum(eta*dist_eta)
+        eta = exp(eta) !/sum(dist_eta*exp(eta))
 
         ! discretize theta shocks
         call discretize_AR(0.920d0**5d0, 0.0d0, sigma5(0.920d0, 0.0375d0), theta, pi_theta, dist_theta)
