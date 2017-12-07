c6e7c0f63cd169ed7a58d84c49562f8bfeade91e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-07 14:29:59 +0100

    edit

--- LCEntrepreneur/main.f90
+++ LCEntrepreneur/main.f90
@@ -418,8 +418,6 @@ contains
 
         enddo
 
-        write(*,*)'siut)'
-
     end subroutine
 
 
@@ -468,7 +466,6 @@ contains
                 enddo
               enddo
             enddo
-            write(*,*)ij, 'agg'
 
             c_coh(0, ij) = c_coh(0, ij)/max(sum(m(:, 0, :, :, :, :, ij)), 1d-13)
             c_coh(1, ij) = c_coh(1, ij)/max(sum(m(:, 1:NK, :, :, :, :, ij)), 1d-13)
