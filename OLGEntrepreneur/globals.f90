35230ae81a9dd69ec1fc0519845a677672d3cab8
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-19 22:56:24 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -179,7 +179,7 @@ module globals
 
         omega_x_t(0, iq_p, ik, ix, ip_p, iw, ie, ij) = 0d0
         omega_k_t(0, iq_p, ik, ix, ip_p, iw, ie, ij) = 0d0
-        S(0, iq_p, ik, ix, ip_p, iw, ie, ij) = -inv_w(0d0)
+        S(0, iq_p, ik, ix, ip_p, iw, ie, ij) = inv_w(0d0)
 
       endif
 
@@ -214,7 +214,7 @@ module globals
 
         omega_x_t(1, iq_p, ik, ix, ip_p, iw, ie, ij) = 0d0
         omega_k_t(1, iq_p, ik, ix, ip_p, iw, ie, ij) = 0d0
-        S(1, iq_p, ik, ix, ip_p, iw, ie, ij) = -inv_e((/1d0, 1d0/))!-1d-13**egam/egam
+        S(1, iq_p, ik, ix, ip_p, iw, ie, ij) = inv_e((/1d0, 1d0/))!-1d-13**egam/egam
 
       endif
 
