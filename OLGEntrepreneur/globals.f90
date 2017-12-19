c6291c9d7dafbf2c8a06e10472d752355874de55
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-19 21:57:50 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -26,7 +26,7 @@ module globals
     integer, parameter :: NK = 12
 
     ! number of points on the annuity asset grid
-    integer, parameter :: NX = 0
+    integer, parameter :: NX = 12
 
     ! number of points on the pension claim grid
     integer, parameter :: NP = 4
@@ -39,14 +39,14 @@ module globals
     real*8, parameter :: egam = 1d0 - 1d0/gamma
     real*8, parameter :: sigma = 0.3d0
     real*8, parameter :: beta = 0.95d0**5
-    real*8, parameter :: mu_b = 0.0d0
+    real*8, parameter :: mu_b = 0.10d0
 
     ! maximum investment in annuities
-    real*8, parameter :: mx_max = 0.5d0
+    real*8, parameter :: mx_max = 0d0
 
     ! capital parameters
     real*8, parameter :: delta_k = 0.06d0
-    real*8, parameter :: xi = 0d0 !1d0/3d0
+    real*8, parameter :: xi = 1d0/3d0
 
     ! production parameters
     real*8, parameter :: Omega = 1d0
