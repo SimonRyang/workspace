e206c20d815931c8e379103f0f3f1a0b79881254
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-19 18:37:56 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -26,7 +26,7 @@ module globals
     integer, parameter :: NK = 0
 
     ! number of points on the annuity asset grid
-    integer, parameter :: NX = 0
+    integer, parameter :: NX = 2
 
     ! number of points on the pension claim grid
     integer, parameter :: NP = 4
