bd9b386b3511e78e0bccad2f54e84adf79cbfe4d
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2017-12-19 22:09:06 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -32,26 +32,26 @@ module globals
     integer, parameter :: NP = 4
 
     ! demographic parameters
-    real*8, parameter :: n_p = (1d0+0.005d0)**5-1d0
+    real*8, parameter :: n_p = 0d0 !(1d0+0.005d0)**5-1d0
 
     ! household preference parameters
     real*8, parameter :: gamma = 0.5d0
     real*8, parameter :: egam = 1d0 - 1d0/gamma
     real*8, parameter :: sigma = 0.3d0
     real*8, parameter :: beta = 0.95d0**5
-    real*8, parameter :: mu_b = 0.10d0
+    real*8, parameter :: mu_b = 0d0 ! 0.10d0
 
     ! maximum investment in annuities
     real*8, parameter :: mx_max = 0.2d0
 
     ! capital parameters
-    real*8, parameter :: delta_k = 0.06d0
-    real*8, parameter :: xi = 1d0/3d0
+    real*8, parameter :: delta_k = 0d0 !0.06d0
+    real*8, parameter :: xi = 0d0 !1d0/3d0
 
     ! production parameters
     real*8, parameter :: Omega = 1d0
     real*8, parameter :: k_min = 0.2d0
-    real*8, parameter :: phi_k = 0.5d0
+    real*8, parameter :: phi_k = 0d0 !0.5d0
     real*8, parameter :: alpha = 0.36d0
     real*8, parameter :: nu = 0.88d0
 
