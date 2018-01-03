ac423ac76e81516614d7a8c4994fb1b336354149
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-03 14:49:36 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -46,7 +46,7 @@ module globals
     real*8, parameter :: mu_b = 0.10d0
 
     ! maximum investment in annuities
-    real*8, parameter :: mx_max = 0.01d0
+    real*8, parameter :: mx_max = 0.10d0
 
     ! capital parameters
     real*8, parameter :: delta_k = 0.06d0
@@ -77,7 +77,7 @@ module globals
     ! size of the annuity grid
     real*8, parameter :: x_l    = Q_l
     real*8, parameter :: x_u    = Q_u
-    real*8, parameter :: x_grow = 4d0*Q_grow
+    real*8, parameter :: x_grow = 0.4d0*Q_grow
 
     ! size of the pension claim grid
     real*8, parameter :: p_l    = 0d0
