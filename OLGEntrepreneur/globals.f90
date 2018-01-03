376e6d69a423821ca0811ce3184a4582b4f89cf3
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-03 22:29:09 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -45,8 +45,8 @@ module globals
   ! household preference parameters
   real*8, parameter :: gamma = 0.5d0
   real*8, parameter :: egam  = 1d0 - 1d0/gamma
-  real*8, parameter :: sigma = 0.330d0
-  real*8, parameter :: beta  = 0.99d0**5
+  real*8, parameter :: sigma = 0.320d0
+  real*8, parameter :: beta  = 0.96d0**5
   real*8, parameter :: mu_b  = 0.01d0
 
   ! maximum investment in annuities
