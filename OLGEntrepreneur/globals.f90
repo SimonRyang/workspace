739f0d04be7f10285acba09afc55f48c8edb6bb7
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-05 09:35:38 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -61,7 +61,7 @@ module globals
   real*8, parameter :: xi      = 1d0/3d0
 
   ! production parameters
-  real*8, parameter :: Omega = 1.4d0
+  real*8, parameter :: Omega = 1.0d0
   real*8, parameter :: k_min = 0.2d0
   real*8, parameter :: phi_k = 0.25d0
   real*8, parameter :: alpha = 0.36d0 !
