66705ffd74c514251ece8dd784c7f5b9305889d8
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-05 11:15:37 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -51,7 +51,7 @@ module globals
   real*8, parameter :: egam  = 1d0 - 1d0/gamma !
   real*8, parameter :: sigma = 0.320d0
   real*8, parameter :: beta  = 0.99d0**5
-  real*8, parameter :: mu_b  = 0.05d0
+  real*8, parameter :: mu_b  = 0.005d0
 
   ! maximum investment in annuities
   real*8, parameter :: mx_max = 0.10d0
