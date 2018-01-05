3ca80cc48e2151ebffbbd1b0b666d7213d7f767a
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-05 09:25:17 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -825,6 +825,7 @@ contains
         write(*,'(a, f10.4)')    '  + corp. tax (%):   ', TAy/(TAc+TAw+TAr+TAy)*100d0
         write(*,'(a, f10.4)')    '- cons. tax rate (%):', tauc*100d0
         write(*,'(a, f10.4)')    '- cap.-output ratio  ', 5*KK/YY
+        write(*,*)
 
     end subroutine
 
