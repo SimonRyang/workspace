3b77959e3a8bf54818d2fbcf0bd76b278b01bf93
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-14 00:59:12 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -128,8 +128,10 @@ contains
       do it = 1, TT
       write(*,*)it, year(it, 1, JJ), sum(c(:, :, :, :, :, :, :, :, it))
       enddo
-      write(*,*)c(0, 0, 0, 0, 1, 1, 1, 1, 49)
-      write(*,*)c(0, 0, 0, 0, 1, 1, 1, 1, 59)
+
+      write(*,*)c(0, 0, 0, 0, 1, 1, 1, 1, it)
+      it = it+50
+      write(*,*)c(0, 0, 0, 0, 1, 1, 1, 1, it)
 
       ! calculate the distribution of households over state space
       do it = 1, TT
