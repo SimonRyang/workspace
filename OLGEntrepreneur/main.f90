f72fa79e7a9a6003acf9c3e389243c1dd644974e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-13 10:25:42 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -145,7 +145,7 @@ contains
       do it = 1, TT
         write(*,'(i4,5f8.2,f14.8)')iter, (/5d0*KK(it), CC(it), II(it)/)/YY(it)*100d0, &
           ((1d0+r(it))**0.2d0-1d0)*100d0, w(it), DIFF(it)/YY(it)*100d0
-        write(*,*) YY(it), CC(it), II(it), GG(it)
+        write(*,*) KC(it), LC(it), YY(it), CC(it), II(it), GG(it)
         enddo
       ! write(*,'(i4,5f8.2,f14.8)')iter, (/5d0*KK(TT), CC(TT), II(TT)/)/YY(TT)*100d0, &
       !    ((1d0+r(TT))**0.2d0-1d0)*100d0, w(TT), DIFF(itmax)/YY(itmax)*100d0
