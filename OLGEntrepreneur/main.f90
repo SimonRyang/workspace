a4aa967d92477cf8fde2f219cc5909f188010b80
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-14 23:12:56 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -145,7 +145,7 @@ contains
 
       ! write screen output
       itmax = maxloc(abs(DIFF(1:TT)/YY(1:TT)), 1)
-      write(*,'(i4,5f8.2,f14.8)')iter, (/5d0*KK(TT), CC(TT), II(TT)/)/YY(TT)*100d0, &
+      write(*,'(i4,i6,5f8.2,f14.8)')iter, itmax, (/5d0*KK(TT), CC(TT), II(TT)/)/YY(TT)*100d0, &
          ((1d0+r(TT))**0.2d0-1d0)*100d0, w(TT), DIFF(itmax)/YY(itmax)*100d0
 
       check = abs(DIFF(itmax)/YY(itmax))*100d0 < tol .and. iter > 20
@@ -315,7 +315,7 @@ contains
     integer :: it
 
     write(*,'(/a/)')'TRANSITION PATH'
-    write(*,'(a)')'ITER     K/Y     C/Y     I/Y       r       w          DIFF'
+    write(*,'(a)')'ITER    IT     K/Y     C/Y     I/Y       r       w          DIFF'
 
     do it = 1, TT
 
