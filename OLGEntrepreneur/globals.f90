85a11a4d7ab73acca66f75f4a119831832c4bf15
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-14 07:54:42 +0100

    edit

--- OLGEntrepreneur/globals.f90
+++ OLGEntrepreneur/globals.f90
@@ -360,8 +360,8 @@ contains
 
     ! solve the household problem using fminsearch
     if (ij < JR) then
-      !call fminsearch(x_in, fret, (/Q_l, 0d0/), (/Q_u, 0.99d0/), cons_o)
-      call fminsearch(x_in(1), fret, Q_l, Q_u, cons_o)
+      call fminsearch(x_in, fret, (/Q_l, 0d0/), (/Q_u, 0.99d0/), cons_o)
+      !call fminsearch(x_in(1), fret, Q_l, Q_u, cons_o)
     else
       call fminsearch(x_in(1), fret, Q_l, Q_u, cons_r)
     endif
@@ -595,7 +595,7 @@ contains
     implicit none
 
     !##### INPUT/OUTPUT VARIABLES ###############################################
-    real*8, intent(in) :: x_in
+    real*8, intent(in) :: x_in(:)
     real*8 :: cons_o
 
     !##### OTHER VARIABLES ######################################################
@@ -603,11 +603,11 @@ contains
     integer :: iql, iqr, ipl, ipr
 
     ! define tomorrow's assets
-    Q_plus  = x_in
+    Q_plus  = x_in(1)
 
     ! define labor supply
-    !lab_com = max(x_in(2), 0d0)
-    lab_com = 0.33d0
+    lab_com = max(x_in(2), 0d0)
+    !lab_com = 0.33d0
 
     ! compute current occupation
     ind_o = abs(dble(ik_com > 0))
