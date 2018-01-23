5984d4d637b66140d782e7196ff5f5eb3b307bfd
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-23 19:07:15 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -18,9 +18,9 @@ program main
   call get_SteadyState()
 
   ! set reforms
-  mu(1:TT) = 1d0
+  ! mu(1:TT) = 1d0
   ! lambda (1:TT) = 1d0
-  ! phi(1:TT) = 1d0
+  phi(1:TT) = 1d0
 
   ! calculate transition path without lsra
   lsra_on = .false.
