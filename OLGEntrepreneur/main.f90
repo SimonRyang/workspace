e51295e373d6778bb10bccdc4b1f46d3638fec6a
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-24 15:11:11 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -16,6 +16,10 @@ program main
 
   r = 0.19d0
   w = 1d0
+
+
+    smopec = .true.
+
   ! calculate initial equilibrium
   call get_SteadyState()
 
@@ -23,10 +27,6 @@ program main
   ! mu(1:TT) = 0d0
   lambda (1:TT) = 1d0
   ! phi(1:TT) = 1d0
-
-
-  smopec = .true.
-
 
   ! calculate transition path without lsra
   lsra_on = .false.
