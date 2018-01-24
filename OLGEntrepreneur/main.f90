adf47e8536dad29cfe44ae885a35b7d4f1022ed4
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-24 16:15:30 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -1093,7 +1093,7 @@ contains
     II(it) = (1d0+n_p)*KK(itp) - (1d0-delta_k)*KK(it)
 
     ! compute gap on goods market
-    DIFF(it) = YY(it)-CC(it)-II(it)-GG(it)-TC(it)
+    DIFF(it) = YY(it)-CC(it)-II(it)-GG(it)-TC(it)-NEX(it)
     write(*,*)DIFF(it), NEX(it)
     write(*,*)r(it), w(it)
 
