ce909c3d8b406dffcfc366f7eeef0e14378f297e
FlorianHo <florian.hofbauer@uni-wuerzburg.de>
2018-01-24 17:16:00 +0100

    edit

--- OLGEntrepreneur/main.f90
+++ OLGEntrepreneur/main.f90
@@ -1062,6 +1062,12 @@ contains
 
     write(*,*) KC(it), BF(it), NEX(it), AA(it), AX(it), BB(it), BA(it), LC(it)
 
+    ! compute gap on goods market
+    DIFF(it) = YY(it)-CC(it)-II(it)-GG(it)-TC(it)-NEX(it)
+
+        write(*,*)DIFF(it), NEX(it)
+        write(*,*)r(it), w(it)
+
   end subroutine
 
 
@@ -1097,11 +1103,6 @@ contains
 
     ! commpute investment
     II(it) = (1d0+n_p)*KK(itp) - (1d0-delta_k)*KK(it)
-
-    ! compute gap on goods market
-    DIFF(it) = YY(it)-CC(it)-II(it)-GG(it)-TC(it)-NEX(it)
-    write(*,*)DIFF(it), NEX(it)
-    write(*,*)r(it), w(it)
 
   end subroutine
 
